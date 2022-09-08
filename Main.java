d1a8a8d3bd567eba8f1f6dafd7db5df79dd38342
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-08 16:19:51 +0000

    Bug fixes

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ class Main {
     System.out.println("You have purchased:"+ dozenNum + " dozen eggs for 3.25 each. This cost:" + dozenCost);
     int dozenRemainder = eggsPurchased % 12;
     double remainderCost = dozenRemainder * 0.45;
-    System.out.println("You have:" + dozenRemainder +" lose eggs. At 45 cents each, that amounts to,"+ remainderCost, " .");
+    System.out.println("You have:" + dozenRemainder +" lose eggs. At 45 cents each, that amounts to,"+ remainderCost + " .");
     
 
 
