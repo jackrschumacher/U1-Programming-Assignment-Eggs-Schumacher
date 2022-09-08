cbf5112f03c62b79d2cf2c0f6c2762f6f6980e24
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-08 16:23:19 +0000

    Clean up output

--- Main.java
+++ Main.java
@@ -9,12 +9,12 @@ class Main {
     System.out.println("You are buying " + eggsPurchased + " eggs");
     int dozenNum = eggsPurchased / 12;
     double dozenCost = dozenNum * 3.25;
-    System.out.println("You have purchased:"+ dozenNum + " dozen eggs for 3.25 each. This cost:" + dozenCost);
+    System.out.println("You have purchased: "+ dozenNum + " dozen eggs for 3.25 each. This cost: " + dozenCost);
     int dozenRemainder = eggsPurchased % 12;
     double remainderCost = dozenRemainder * 0.45;
     System.out.println("You have:" + dozenRemainder +" lose eggs. At 45 cents each, that amounts to, $"+ remainderCost + " Dollars.");
     double total = remainderCost + dozenCost;
-    System.out.println("Your total cost is:"+total+" Dollars");
+    System.out.println("Your total cost is: "+total+" Dollars");
 
 
 
