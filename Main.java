1f15e9cce387ed8974327c32bb3ec2ae58a6830b
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-08 16:07:36 +0000

    Clean up code

--- Main.java
+++ Main.java
@@ -4,19 +4,12 @@ class Main {
   public static void main(String[] args) {
     //Create Scanner obj to get input
     Scanner input = new Scanner(System.in);
-
     System.out.println("How many eggs would you like to purchase?");
-
     int eggsPurchased = input.nextInt();
-    
     System.out.println("You are buying " + eggsPurchased + " eggs");
-
     int dozenNum = eggsPurchased / 12;
-
     System.out.println(dozenNum);
-
     int dozenRemainder = eggsPurchased % 12;
-
     System.out.println(dozenRemainder);
 
 
