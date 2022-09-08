e279b46e409558ce831e1b28c312a7e3a9f2b09e
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-08 16:19:35 +0000

    Begin Integrate Cost

--- Main.java
+++ Main.java
@@ -8,9 +8,12 @@ class Main {
     int eggsPurchased = input.nextInt();
     System.out.println("You are buying " + eggsPurchased + " eggs");
     int dozenNum = eggsPurchased / 12;
-    System.out.println(dozenNum);
+    double dozenCost = dozenNum * 3.25;
+    System.out.println("You have purchased:"+ dozenNum + " dozen eggs for 3.25 each. This cost:" + dozenCost);
     int dozenRemainder = eggsPurchased % 12;
-    System.out.println(dozenRemainder);
+    double remainderCost = dozenRemainder * 0.45;
+    System.out.println("You have:" + dozenRemainder +" lose eggs. At 45 cents each, that amounts to,"+ remainderCost, " .");
+    
 
 
 
