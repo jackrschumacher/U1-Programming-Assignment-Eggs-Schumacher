8f11d483bd09b4f154423691313714bb9e00b193
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-08 15:51:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    //Create Scanner obj to get input
+    Scanner input = new Scanner(System.in);
+    
+
+
+
+
+
+
+    
+  }
+}
