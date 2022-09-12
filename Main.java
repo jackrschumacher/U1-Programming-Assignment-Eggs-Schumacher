91eb9393313658877ddf2734406bc738408e4aa3
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-12 15:48:56 +0000

    Comments

--- Main.java
+++ Main.java
@@ -7,11 +7,11 @@ class Main {
     System.out.println("How many eggs would you like to purchase?"); //user inputs number of eggs they wish to purchase
     int eggsPurchased = input.nextInt();
     System.out.println("You are buying " + eggsPurchased + " eggs");
-    int dozenNum = eggsPurchased / 12;
+    int dozenNum = eggsPurchased / 12; //Divide Number of Eggs by 12 to get number that will fit in a dozen
     double dozenCost = dozenNum * 3.25;
     System.out.println("You have purchased: "+ dozenNum + " dozen eggs for 3.25 each. This cost: $" + dozenCost);
-    int dozenRemainder = eggsPurchased % 12;
-    double remainderCost = dozenRemainder * 0.45;
+    int dozenRemainder = eggsPurchased % 12; //Mod remainder in order to find num left over
+    double remainderCost = dozenRemainder * 0.45; //multiply by 0.45 cents to get cost
     System.out.println("You have:" + dozenRemainder +" lose eggs. At 45 cents each, that amounts to, $"+ remainderCost + " Dollars.");
     double total = remainderCost + dozenCost;
     System.out.println("Your total cost is: $"+total+" Dollars");
